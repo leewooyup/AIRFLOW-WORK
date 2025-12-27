2c606bd70b1be2b414a6663e6bef98f6fc3822d5
woo yup Lee <zjadlspun1114@naver.com>
2025-12-27 16:57:01 +0900

    fix: dag_id 중복 수정

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -5,7 +5,7 @@ import pendulum
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="dags_conn_test",
     schedule="10 1 L * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
     catchup=False,
