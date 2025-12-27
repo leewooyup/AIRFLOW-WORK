37736bc2cf06323efdd7e7a255673fb138ccab0a
woo yup Lee <zjadlspun1114@naver.com>
2025-12-27 16:41:09 +0900

    fix: cron 스케줄 표현식 맞추기

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -6,7 +6,7 @@ from airflow.operators.empty import EmptyOperator
 
 with DAG(
     dag_id="dags_bash_operator",
-    schedule="None",
+    schedule="10 1 L * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
     catchup=False,
     tags=["example", "example2", "example3"],
