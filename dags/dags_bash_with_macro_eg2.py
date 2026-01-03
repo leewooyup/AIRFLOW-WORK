cc7fbc1eb87248ad1cfb74b8005d799bb6a97987
woo yup Lee <zjadlspun1114@naver.com>
2026-01-03 19:30:27 +0900

    correct parentheses

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -13,7 +13,7 @@ with DAG(
     # START_DATE: 2주전 월요일, END_DATE: 2주전 토요일
     bash_task_2 = BashOperator(
         task_id='bask_task_2',
-        env={'START_DATE': '{{ data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds }}', # 이전 배치에 돌았던 날짜
+        env={'START_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds }}', # 이전 배치에 돌았던 날짜
              'END_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds }}'
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
